2850e02033afc3c6e7563a0432976c0adfcc6991
Md. Sajib Hosen <sajibcu55@gmail.com>
2025-06-20 10:06:16 +0800

    product  validation added

--- productservice/src/main/java/com/learn/productservice/model/Product.java
+++ productservice/src/main/java/com/learn/productservice/model/Product.java
@@ -4,24 +4,30 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Product name cannot be blank")
     private String name;
     private String description;
+
     private Double price;
+
+    @Min(0)
     private Integer quantity;
     private Long addedBy;
 }
